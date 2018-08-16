7ce8db55cd70dfba782304b8336824e7cd8b9c1a
nirkaufman <nirkaufman@gmail.com>
2018-08-16 16:14:42 -0400

    cleanup

--- src/app/components/footer.component.ts
+++ src/app/components/footer.component.ts
@@ -6,11 +6,6 @@ import {Component} from '@angular/core';
     <footer class="pt-4 my-md-5 pt-md-5 border-top">
       <div class="row">
         <div class="col-12 col-md">
-          <img class="mb-2"
-               src="../../assets/brand/bootstrap-solid.svg"
-               alt=""
-               width="24"
-               height="24">
           <small class="d-block mb-3 text-muted">Nir Kaufman &copy; 2018</small>
         </div>
       </div>
